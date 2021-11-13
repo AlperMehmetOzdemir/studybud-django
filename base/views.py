d51d81e18d3ce5d6e9ec08919f2f7b760e947e8b
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-11-13 16:23:58 +0300

    Addded individual acitivities and topics pages

--- base/views.py
+++ base/views.py
@@ -67,7 +67,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q) 
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     activity_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -201,3 +201,20 @@ def updateUser(request):
     }
 
     return render(request, "base/update_user.html", context)
+
+def topicsPage(request):
+    q = request.GET.get("g") if request.GET.get("q") != None else ""
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {
+        "topics": topics
+    }
+    return render(request, "base/topics.html", context)
+
+def activitiesPage(request):
+    activity_messages = Message.objects.all();
+    
+    context = {
+        "activity_messages": activity_messages
+    }
+
+    return render(request, "base/activity.html", context)
